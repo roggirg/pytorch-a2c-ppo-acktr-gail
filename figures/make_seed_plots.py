6b7fefcb3f30c93f3c14d8f48e77b6a35f62b2ae
roggirg <roger.girgis@gmail.com>
2020-03-27 10:45:07 -0400

    Modified code to plot seed curves with running averages.

--- figures/make_seed_plots.py
+++ figures/make_seed_plots.py
@@ -6,20 +6,28 @@ labels = {
     'normal': {'fnames': "CarEnv-Chaos-v0_normal_s", "color": 'r'},
     'noIntent': {'fnames': "CarEnv-Chaos-v0_no_intention_s", "color": 'b'},
     '8_ped': {'fnames': "CarEnv-Chaos-v0_8_ped_s", "color": 'g'},
-    'Rped200': {'fnames': "None", "color": 'y'},
+    'Rped200': {'fnames': "CarEnv-Chaos-v0_rped200_s", "color": 'y'},
     'noVelR': {'fnames': "CarEnv-Chaos-v0_no_vel_s", "color": 'c'}
 }
 
+
+def running_mean(x, N):
+    cumsum = np.cumsum(np.insert(x, 0, 0))
+    return (cumsum[N:] - cumsum[:-N]) / float(N)
+
+
 for key, vals in labels.items():
-    if key not in ["normal", "noIntent", "8_ped", "noVelR"]:
+    if key not in ["normal", "noIntent", "8_ped", "noVelR", "Rped200"]:
         continue
     data = np.array([np.load("data/"+vals['fnames']+"0.npy")[:4879],
                      np.load("data/"+vals['fnames']+"1.npy")[:4879],
                      np.load("data/"+vals['fnames']+"2.npy")[:4879]])
 
-    mean = np.mean(data, axis=0)
-    std = np.std(data, axis=0)
-    plt.fill_between(range(len(mean)), mean - std, mean + std, alpha=0.5, facecolor=vals['color'], label=key)
+    mean = running_mean(np.mean(data, axis=0), 50)
+    std = running_mean(np.std(data, axis=0), 50)
+    # plt.fill_between(range(len(mean)), mean - std, mean + std, alpha=0.5, facecolor=vals['color'], label=key)
+    plt.fill_between(range(len(mean)), mean - std, mean + std, alpha=0.1, facecolor=vals['color'])
+    plt.plot(range(len(mean)), mean, vals['color'], label=key)
     # plt.plot(mean, color=vals['color'], label=key)
 
 plt.legend()
